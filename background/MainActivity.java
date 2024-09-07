1774fd684460e10e1cddb29db38c0e15e659feba
Jacob KM <144668246+JACOBKM1@users.noreply.github.com>
2024-09-07 11:26:16 +0530

    Create MainActivity.java

---
+++ background/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.background;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+//0xff0000ff
+
+    public void click(View view) {
+        getWindow().getDecorView().setBackgroundColor(0xff0000ff);
+    }
+}
+
+
